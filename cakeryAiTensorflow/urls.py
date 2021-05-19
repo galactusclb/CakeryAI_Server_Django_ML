61cc3a12ccd41567c3eeaea2878c7a2621639349
galactusclb <chanakabalasuriya123@gmail.com>
2021-05-19 11:33:23 +0530

    v1.0.10 (deploy to heroku)

--- cakeryAiTensorflow/urls.py
+++ cakeryAiTensorflow/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.static import serve
+from django.conf.urls import url
+
 from predictApp import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app/', include('predictApp.urls'))
+    path('app/', include('predictApp.urls')),
+
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
